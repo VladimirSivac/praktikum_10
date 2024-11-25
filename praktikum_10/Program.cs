82f30c06235a54e2dd8a48f2ab822bd802d80f24
VladimirSivac <morndes@gmail.com>
2024-11-25 19:19:43 +0200

    task_2 done

--- praktikum_10/Program.cs
+++ praktikum_10/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace praktikum_10
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            double x = Func(5, 7) + Func(12, 8) + Func(19, 2);
+            Console.WriteLine($"x = {x:f2}");
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Function
+        /// </summary>
+        /// <param name="num">num1, num2</param>
+        /// <returns>x</returns>
+        static double Func(double num1, double num2)
+        {
+            double x = (Math.Sqrt(num1) + num1) / (Math.Sqrt(num2) + num2);
+            return x;
         }
     }
 }
