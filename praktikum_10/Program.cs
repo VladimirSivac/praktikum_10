5852d291ce49b7bd12aba20abbd28b1adadab3d2
VladimirSivac <morndes@gmail.com>
2024-11-25 19:15:35 +0200

    task_1 done

--- praktikum_10/Program.cs
+++ praktikum_10/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace praktikum_10
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            double x = Func(5) + Func(12) + Func(19);
+            Console.WriteLine($"x = {x:f2}");
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Function
+        /// </summary>
+        /// <param name="num">num</param>
+        /// <returns>x</returns>
+        static double Func(double num)
+        {
+            double x = (Math.Sqrt(num) + num) / 2;
+            return x;
         }
     }
 }
