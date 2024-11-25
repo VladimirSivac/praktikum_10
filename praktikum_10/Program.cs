c77be5895a0986beee0dbac80e916f785cb51138
VladimirSivac <morndes@gmail.com>
2024-11-25 19:46:49 +0200

    task_3 done

--- praktikum_10/Program.cs
+++ praktikum_10/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace praktikum_10
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.Write("a = ");
+            int a = Input();
+            Console.Write("b = ");
+            int b = Input();
+            Console.WriteLine($"Максимальное число - {Max(a, b)}");
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Input
+        /// </summary>
+        /// <returns>a</returns>
+        static int Input()
+        { 
+            int a = Convert.ToInt32(Console.ReadLine());
+            return a;
+        }
+        /// <summary>
+        /// MaxValue
+        /// </summary>
+        /// <param name="a">int a</param>
+        /// <param name="b">int b</param>
+        /// <returns>max-param</returns>
+        static int Max(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
         }
     }
 }
