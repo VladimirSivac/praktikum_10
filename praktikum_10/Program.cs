5fad5fb43318330e517b34287b9202d9b1fa7796
VladimirSivac <morndes@gmail.com>
2024-11-25 20:11:40 +0200

    task_5 done

--- praktikum_10/Program.cs
+++ praktikum_10/Program.cs
@@ -10,6 +10,33 @@ namespace praktikum_10
     {
         static void Main(string[] args)
         {
+            Console.Write("a = ");
+            int a = Input();
+            Console.Write("b = ");
+            int b = Input();
+            Console.Write("c = ");
+            int c = Input();
+            Console.Write("d = ");
+            int d = Input();
+            int s = Max(a, b) + Max(c, d);
+            Console.Write($"s = {Max(a, b)} + {Max(c, d)} = {s}");
+            Console.ReadLine();
+        }
+        static int Input()
+        { 
+            int a = Convert.ToInt32(Console.ReadLine());
+            return a;
+        }
+        static int Max(int a, int b)
+        { 
+            if (a > b)
+            { 
+                return a;
+            } 
+            else
+            { 
+                return b; 
+            } 
         }
     }
 }
